57969b4356a1e66fcbc6d1d0b1d8834770e7bfad
zzg <zhiguo-zhang@163.com>
2018-08-07 16:42:37 +0800

    2

---
+++ A/A/Targets/Target_A.h
@@ -0,0 +1,19 @@
+//
+//  Target_A.h
+//  A
+//
+//  Created by zhangzhiguo on 2018/8/7.
+//  Copyright © 2018年 zhangzhiguo. All rights reserved.
+//
+
+#ifndef Target_A_h
+#define Target_A_h
+
+#import <UIKit/UIKit.h>
+
+@interface Target_A : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+
+@end
+#endif /* Target_A_h */
---
+++ A/A/Targets/Target_A.m
@@ -0,0 +1,21 @@
+//
+//  Target_A.m
+//  A
+//
+//  Created by zhangzhiguo on 2018/8/7.
+//  Copyright © 2018年 zhangzhiguo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Target_A.h"
+#import "AViewController.h"
+
+@implementation Target_A
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params
+{
+    AViewController *viewController = [[AViewController alloc] init];
+    return viewController;
+}
+
+@end
